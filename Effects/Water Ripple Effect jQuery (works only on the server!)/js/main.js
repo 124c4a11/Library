60df1cc5fd92581fc4d4a5164744a2e24b5ceb01
124c4a11 <124c4a11@mail.ru>
2018-03-14 14:44:54 +0300

    Water Ripple Effect jQuery (works only on the server!)

--- Effects/Water Ripple Effect jQuery (works only on the server!)/js/main.js
+++ Effects/Water Ripple Effect jQuery (works only on the server!)/js/main.js
@@ -4,7 +4,6 @@
 $(document).ready(function() {
   if ($('.hero').length) {
     $('.hero').ripples({
-      igeUrl: 'img/1.jpg',
       resolution: 512,
       dropRadius: 20,
       perturbance: 0.04
