713a6f245aacfef978108f11b01904cc168a70a5
124c4a11 <124c4a11@mail.ru>
2018-04-03 16:58:58 +0300

    Animated Typing (Typed.js)

---
+++ Typography Effects/Animated Typing (Typed.js)/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+window.onload = () => {
+  new Typed('.type', {
+    strings: [
+      'I Love HTML',
+      'I Love CSS',
+      'I Love JS'
+    ],
+    showCursor: false,
+    smartBackspace: false,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  });
+};
