c6550ca34c036b00aae8abff112713d36691a747
124c4a11 <124c4a11@mail.ru>
2019-10-30 01:38:43 +0300

    Scrollable Section (PagePiling.js)

---
+++ Carousels/Scrollable Section (PagePiling.js)/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+    navigation: {
+      'textColor': '#000',
+      'bulletsColor': '#fff',
+      'position': 'right',
+      'tooltips': ['Section 1', 'Section 2', 'Section 3', 'Section 4']
+    },
+  });
+});
