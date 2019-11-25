46b945136a514826db3ca3869af83a0a6451a5b3
124c4a11 <124c4a11@mail.ru>
2019-11-25 16:12:23 +0300

    Scratch Effect

---
+++ Backgrounds/Scratch Effect/js/app.js
@@ -0,0 +1,5 @@
+$('#scratch').wScratchPad({
+  size        : 75,          // The size of the brush/scratch.
+  bg          : 'https://picsum.photos/id/354/1920/1200',  // Background (image path or hex color).
+  fg          : '#000',  // Foreground (image path or hex color).
+});
